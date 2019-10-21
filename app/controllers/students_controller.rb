231e04eb4a34ab368afa9dc964925701feeb28ed
Will Hodgson <will.hodgson@current-rms.com>
2019-10-21 09:36:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,11 +9,12 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.last
   end
 
   def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    @student = Student.last
+
     redirect_to new_student_path
   end
 
